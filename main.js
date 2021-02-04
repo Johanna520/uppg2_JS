3e84b871078d3a4fd6a548103fb9214456bb50e8
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2021-02-04 14:56:14 +0100

    checkbox ok

--- main.js
+++ main.js
@@ -1,11 +1,45 @@
 "strict mode";
 
-//search-city-imput
-const input = document.querySelector('input');
-const log = document.getElementById('values');
+/*
+search-city
+i min sök-input skriver jag in den stadjag vill söka efter. 
+jag använder mig av event "change". när användaren trycker på 
+Enter ändras id="city" till det användaren skriver in i sökfältet. 
 
-input.addEventListener('input', updateValue);
+*/
+{
+let input = document.getElementById('executeSearch');
+let log = document.getElementById('city');
+
+input.addEventListener('change', updateValue);
 
 function updateValue(e) {
   log.textContent = e.target.value;
 }
+}
+/*
+fetch('api.openweathermap.org/data/2.5/weather?q={city name}&appid=a0d70789704075e6099712d8603723f3 ') hämta api*/ 
+
+
+{
+    document.getElementById("checkBox1").onchange = function(e){
+        var text = document.getElementById("textWeather");
+        if (checkBox1.checked == true){
+            text.style.display = "block";
+          } else {
+             text.style.display = "none";
+          }
+    }
+    }
+
+
+    {
+        document.getElementById("checkBox2").onchange = function(e){
+            var text = document.getElementById("textAttractions");
+            if (checkBox2.checked == true){
+                text.style.display = "block";
+              } else {
+                 text.style.display = "none";
+              }
+        }
+        }
