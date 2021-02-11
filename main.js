3d3a767e8b5aa2bfe03eb495572823a3e82945a0
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2021-02-11 14:58:59 +0100

    städat kod och start fetch()

--- main.js
+++ main.js
@@ -1,25 +1,7 @@
 "strict mode";
 
-/*
-search-city
-i min sök-input skriver jag in den stadjag vill söka efter. 
-jag använder mig av event "change". när användaren trycker på 
-Enter ändras id="city" till det användaren skriver in i sökfältet. 
 
-*/
-
-let input = document.querySelector('#executeSearch');
-let outputElement = document.querySelector('#city');
-
-input.onchange = function cityName(e) {
-  outputElement.innerText = e.target.value;
-}
-
-
-//City name = getCityWeatherURL (Se freläsnignen kl 11.00)
-
-/*för att man ska kunna få fram informationen  från checkbox ang väder. 
-*/
+//för att man ska kunna få fram informationen  från checkbox ang väder. 
 {
     document.getElementById("checkBox1").onchange = function showWeather (){
         let text = document.getElementById("textWeather");
@@ -31,9 +13,7 @@ input.onchange = function cityName(e) {
     }
 }
 
-/*för att man ska kunna få fram informationen  från checkbox ang attraktioner. 
-*/
-
+//för att man ska kunna få fram informationen  från checkbox ang attraktioner. 
 {
     document.getElementById("checkBox2").onchange = function showAttractions (){
         let text = document.getElementById("textAttractions");
@@ -45,18 +25,63 @@ input.onchange = function cityName(e) {
     }
 }
 
+let input = document.querySelector('#executeSearch');
+let outputElement = document.querySelector("#textWeather");
 
-/*
-fetch('api.openweathermap.org/data/2.5/weather?q={city name}&appid=a0d70789704075e6099712d8603723f3 ') hämta api*/ 
+function getCityWeatherUrl(input) {
+  const url = new URL("https://api.openweathermap.org/data/2.5/weather");
+
+  url.searchParams.append("q", input.value);
+  url.searchParams.append("appid", "a0d70789704075e6099712d8603723f3");
+  url.searchParams.append("mode", "json");
+  url.searchParams.append("units", "metric");
+  url.searchParams.append("lang", "se");
+
+  outputElement.innerText = url;
+
+  return url;
+}
+const url = getCityWeatherUrl(input);
+window.url = url;
 
 
-//if - else if om ett land som inte finns med på listan = error
+input.onchange = async function() {
+    
+    const url = getCityWeatherUrl(input);
+
+    
+    const serverResponse = await fetch(url); 
+   
+    const info = await serverResponse.json(); 
+  
+
+    outputElement.innerText = url;
+    
+  }
 
 
-/* 
-något sånt här för att få ut max 10 populära sevärdigheter?
-se föreläsning ca kl 10
+
+
+//för att man ska kunna få fram informationen  från checkbox ang väder. 
+{
+    document.getElementById("checkBox1").onchange = function showWeather (){
+        let text = document.getElementById("textWeather");
+        if (checkBox1.checked === true){
+            text.style.display = "block";
+          } else {
+             text.style.display = "none";
+          }
+    }
 }
 
-
-*/
+//för att man ska kunna få fram informationen  från checkbox ang attraktioner. 
+{
+    document.getElementById("checkBox2").onchange = function showAttractions (){
+        let text = document.getElementById("textAttractions");
+        if (checkBox2.checked === true){
+            text.style.display = "block";
+        } else {
+            text.style.display = "none";
+        }
+    }
+}
